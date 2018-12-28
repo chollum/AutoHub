2b0bb83374529acf698a4bad836327377b584d81
Collum <holmec5@rpi.edu>
2018-12-28 15:35:23 -0500

    Added homepage, navbar.

--- AutoHub/settings.py
+++ AutoHub/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hub',
     'bootstrap4',
+    'fontawesome',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- AutoHub/urls.py
+++ AutoHub/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='AutoHub/home.html'), name='home'),
     path('', include('hub.urls')),
 ]
