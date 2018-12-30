20f90fc9928af716df7dda8a14a1b78239a0fb06
Collum <holmec5@rpi.edu>
2018-12-30 18:17:11 -0500

    Removed 'add new' tab. Improved homepage. Linked templates for data,lights,doors

--- hub/urls.py
+++ hub/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('doors/', views.doors, name='doors'),
+    path('lights/', views.lights, name='lights'),
+    path('data/', views.data, name='data'),
 ]
--- hub/views.py
+++ hub/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'hub/index.html')
+
+def doors(request):
+    return render(request, 'hub/doors.html')
+
+def lights(request):
+    return render(request, 'hub/lights.html')
+
+def data(request):
+    return render(request, 'hub/data.html')
